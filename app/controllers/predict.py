d53cf80b0c7a30f02fc0aaa4aaedb6a8e988c887
rayyannur5 <rayyannur5@gmail.com>
2024-12-15 07:53:01 +0000

    update from idx

--- app/controllers/predict.py
+++ app/controllers/predict.py
@@ -2,10 +2,10 @@ from transformers import pipeline, AutoModelForSequenceClassification, AutoToken
 
 # model = AutoModelForSequenceClassification.from_pretrained("app\models\\topic")
 # tokenizer = AutoTokenizer.from_pretrained("app\models\\topic")
-topic_model = pipeline("sentiment-analysis", model="app\models\\topic")
+topic_model = pipeline("sentiment-analysis", model="app/models/topic")
 
 
-sentiment_model = pipeline("sentiment-analysis", model="app\models\sentiment")
+sentiment_model = pipeline("sentiment-analysis", model="app/models/sentiment")
 
 res = sentiment_model('BARU satu bulan menjabat, Wakil Presiden Gibran Rakabuming Raka aktif bagi-bagi bantuan sosial atau bansos. Hal yang jadi sorotan adalah penyematan label di kantong tas bansos: Bantuan Wapres Gibran. ')
 
